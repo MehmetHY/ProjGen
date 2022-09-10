b82ed18f59f5bd557a83547af4bdf3d4264d1a2e
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-09-10 12:23:10 +0300

    implemented csharp export

--- ProjGen.Exporting.CSharp/CSharpExporter.cs
+++ ProjGen.Exporting.CSharp/CSharpExporter.cs
@@ -19,6 +19,7 @@ public class CSharpExporter : IExporter
 
         CreateRootDirectory();
         CreateProjectFiles();
+        CreateUnits();
     }
 
     private void CreateRootDirectory()
@@ -41,7 +42,7 @@ public class CSharpExporter : IExporter
             sb.Append(@"<Project Sdk=""Microsoft.NET.Sdk"">")
               .AppendLine()
               .AppendLine(@"  <PropertyGroup>");
-            
+
             if (assembly.Type == AssemblyModel.AssemblyType.Executable)
                 sb.AppendLine(@"    <OutputType>Exe</OutputType>");
 
@@ -91,4 +92,249 @@ public class CSharpExporter : IExporter
             File.WriteAllText(file, content);
         }
     }
+
+    private void CreateUnits()
+    {
+        foreach (var assembly in _assemblies)
+        {
+            var rootDir = Path.Combine(_rootPath, assembly.Name!);
+
+            foreach (var dir in assembly.OfTypeInChildren<DirectoryModel>())
+                CreateDirectory(dir, rootDir);
+
+            foreach (var unit in assembly.OfTypeInChildren<UnitModel>())
+                CreateUnitFile(unit, rootDir);
+        }
+
+    }
+
+    private static void CreateDirectory(DirectoryModel directory,
+                                        string rootDir)
+    {
+        var dir = Path.Combine(rootDir, directory.Name!);
+        Directory.CreateDirectory(dir);
+
+        foreach (var unit in directory.OfTypeInChildren<UnitModel>())
+            CreateUnitFile(unit, dir);
+
+        foreach (var subDir in directory.OfTypeInChildren<DirectoryModel>())
+            CreateDirectory(subDir, dir);
+    }
+
+    private static void CreateUnitFile(UnitModel unit, string rootDir)
+    {
+        var file = Path.Combine(rootDir, $"{unit.Name!}.cs");
+        var content = UnitToString(unit);
+        File.WriteAllText(file, content);
+    }
+
+    public static string TypeToString(TypeModel? model)
+    {
+        if (model == null)
+            return "void";
+
+        var sb = new StringBuilder();
+        sb.Append(model.Name);
+
+        if (model.Generics.Count > 0)
+        {
+            sb.Append('<');
+
+            for (int i = 0; i < model.Generics.Count; ++i)
+            {
+                if (i > 0)
+                    sb.Append(", ");
+
+                var genericType = TypeToString(model.Generics[i]);
+                sb.Append(genericType);
+            }
+
+            sb.Append('>');
+        }
+
+        var typeStr = sb.ToString();
+
+        return typeStr;
+    }
+
+    public static string TypesToString(List<TypeModel> models)
+    {
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < models.Count; ++i)
+        {
+            if (i > 0)
+                sb.Append(", ");
+
+            var typeStr = TypeToString(models[i]);
+            sb.Append(typeStr);
+        }
+
+        var typesStr = sb.ToString();
+
+        return typesStr;
+    }
+
+    public static string VariableToString(VariableModel model)
+    {
+        var type = TypeToString(model.Type!);
+        var variableStr = $"{type} {model.Name}";
+
+        return variableStr;
+    }
+
+    public static string VariablesToString(List<VariableModel> models)
+    {
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < models.Count; ++i)
+        {
+            if (i > 0)
+                sb.Append(", ");
+
+            var variableStr = VariableToString(models[i]);
+            sb.Append(variableStr);
+        }
+
+        var variablesStr = sb.ToString();
+
+        return variablesStr;
+    }
+
+    public static string PropertyToString(PropertyModel model,
+                                          bool accessModifiers = true)
+    {
+        var sb = new StringBuilder();
+
+        if (accessModifiers)
+        {
+            if (!model.HasGet && !model.HasSet)
+                sb.Append("private ");
+            else
+                sb.Append("public ");
+        }
+
+        var typeStr = TypeToString(model.Type!);
+        sb.Append(typeStr)
+          .Append(' ')
+          .Append(model.Name);
+
+
+        if (model.HasGet && model.HasSet)
+            sb.Append(" { get; set; }");
+        else if (model.HasGet)
+        {
+            if (accessModifiers)
+                sb.Append(" { get; private set; }");
+            else
+                sb.Append(" { get; }");
+        }
+        else if (model.HasSet)
+        {
+            if (accessModifiers)
+                sb.Append(" { private get; set; }");
+            else
+                sb.Append(" { set; }");
+        }
+        else
+            sb.Append(';');
+
+        var propStr = sb.ToString();
+
+        return propStr;
+    }
+
+    public static string MethodToString(MethodModel model,
+                                        bool accessModifiers = true)
+    {
+        var sb = new StringBuilder();
+        
+        if (accessModifiers)
+            sb.Append("public ");
+
+        var returnTypeStr = TypeToString(model.ReturnType);
+        sb.Append(returnTypeStr)
+          .Append(' ')
+          .Append(model.Name);
+
+        if (model.GenericTypes.Count > 0)
+        {
+            sb.Append('<');
+            var genericStr = TypesToString(model.GenericTypes);
+            sb.Append(genericStr)
+              .Append('>');
+        }
+
+        sb.Append('(');
+
+        if (model.Args.Count > 0)
+        {
+            var argsStr = VariablesToString(model.Args);
+            sb.Append(argsStr);
+        }
+
+        sb.Append(')');
+        var methodStr = sb.ToString();
+
+        return methodStr;
+    }
+
+    private static string UnitToString(UnitModel model)
+    {
+        var sb = new StringBuilder();
+        sb.Append("namespace ");
+        var ns = model.GenerateNamespace();
+        sb.Append(ns)
+          .AppendLine(";")
+          .AppendLine()
+          .Append("public ");
+
+        if (model.Type == UnitModel.UnitType.Interface)
+            sb.Append("interface ");
+        else
+            sb.Append("class ");
+
+        sb.Append(model.Name);
+
+        if (model.GenericTypes.Count > 0)
+        {
+            sb.Append('<');
+            var genericStr = TypesToString(model.GenericTypes);
+            sb.Append(genericStr)
+              .Append('>');
+        }
+
+        if (model.Inherits.Count > 0)
+        {
+            sb.Append(" : ");
+            var inheritStr = TypesToString(model.Inherits);
+            sb.Append(inheritStr);
+        }
+
+        sb.AppendLine("\n{");
+        var isInterface = model.Type == UnitModel.UnitType.Interface;
+
+        foreach (var prop in model.OfTypeInChildren<PropertyModel>())
+        {
+            var propStr = PropertyToString(prop, !isInterface);
+            sb.AppendLine($"    {propStr}");
+        }
+
+        foreach (var method in model.OfTypeInChildren<MethodModel>())
+        {
+            var methodStr = MethodToString(method, !isInterface);
+            sb.Append($"\n    {methodStr}");
+
+            if (isInterface)
+                sb.AppendLine(";");
+            else
+                sb.AppendLine("\n    {\n\n    }");
+        }
+
+        sb.AppendLine("}");
+        var unitStr = sb.ToString();
+
+        return unitStr;
+    }
+
 }
