ee07f2b4bbba99dd730a6495f8eb6208dc1f8c3c
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-09-10 15:10:54 +0300

    create example cli app

---
+++ Examples/CSharpGenCli/Program.cs
@@ -0,0 +1,49 @@
+﻿using ProjGen.Core;
+using ProjGen.Exporting.CSharp;
+using ProjGen.Utils.Extensions;
+
+var argStr = string.Join(' ', args);
+
+string inputFile = string.Empty;
+string exportDir = string.Empty;
+
+argStr.RegexMatches(@"(?<option>\w) ""?(?<value>.*?)""?(?= *-|$)").Map(match =>
+{
+    var option = match.Groups["option"].Value;
+    var value = match.Groups["value"].Value.Trim('"').Trim('\'').Trim('`');
+
+    switch (option)
+    {
+        case "f":
+            inputFile = value;
+            break;
+
+        case "o":
+            exportDir = value;
+            break;
+    }
+});
+
+if (string.IsNullOrWhiteSpace(inputFile) ||
+    string.IsNullOrWhiteSpace(exportDir))
+{
+    Console.WriteLine(
+        "Bad commandline arguments. Example: '-f inputFile -o exportDir'"
+    );
+
+    return;
+}
+
+var context = Context.Builder.Create()
+                             .SetExporter<CSharpExporter>()
+                             .Build();
+
+try
+{
+    context.Import(inputFile);
+    context.Export(exportDir);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
